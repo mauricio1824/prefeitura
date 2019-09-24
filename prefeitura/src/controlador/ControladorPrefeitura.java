fc2d599f20674daab86c6685db0b116f52f69578
Administrador <Administrador@10.10.4.4>
2019-09-24 16:33:19 -0300

    versao 1

---
+++ prefeitura/src/controlador/ControladorPrefeitura.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import tela.manutencao.ManutencaoPrefeitura;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorPrefeitura {
+
+    public static void inserir(ManutencaoPrefeitura aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
